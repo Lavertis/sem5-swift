56295b3ee53cca245c86ce6975954aaa6cdf2e60
lavertis <29688908+Lavertis@users.noreply.github.com>
2021-10-28 23:14:28 +0200

    fix(lab_4): add more data validation

--- Sources/swift-lab/lab_4.swift
+++ Sources/swift-lab/lab_4.swift
@@ -4,10 +4,10 @@ struct lab_4 {
     static func task_1() {
         print("Podaj rok:")
         guard let year = Int(readLine()!) else {
-            fatalError("To nie jest poprawny rok")
+            fatalError("Podano bledny rok")
         }
 
-        if year % 4 == 0 {
+        if (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 {
             print("Rok jest przestepny")
         } else {
             print("Rok nie jest przestepny")
@@ -17,9 +17,11 @@ struct lab_4 {
     static func task_2() {
         let roman = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
                      "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
-                     "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX"]
+                     "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX",
+                     "XXXI"]
+        print("Podaj rok:")
         guard let year = Int(readLine()!) else {
-            fatalError("To nie jest poprawny rok")
+            fatalError("Podano bledny rok")
         }
 
         let remainder = year % 100
@@ -30,35 +32,37 @@ struct lab_4 {
         } else {
             century = roman[a - 1]
         }
-        print(century)
+        print("Wiek: \(century)")
     }
 
     static func task_3() {
+        print("Podaj srednia:")
         guard let average = Double(readLine()!) else {
-            fatalError("Niepoprawna srednia")
+            fatalError("Podano niepoprawna srednia")
         }
-        var points: Int
+        let scholarship: Int
 
         switch average {
-        case 4.5...: points = 200
-        case 4.0..<4.5: points = 150
-        case 3.0..<4.0: points = 100
-        case ..<3.0: points = 0
-        default: points = 0
+        case 4.5...5: scholarship = 200
+        case 4.0..<4.5: scholarship = 150
+        case 3.0..<4.0: scholarship = 100
+        case 2.0..<3.0: scholarship = 0
+        default: fatalError("Srednia nie nalezy do zakresu")
         }
 
-        print("Punkty: \(points)")
+        print("Punkty: \(scholarship)")
     }
 
     static func task_4() {
         print("Podaj pierwsza liczbe:")
         guard let a = Double(readLine()!) else {
-            fatalError("Podaj poprawna liczbe")
+            fatalError("Podano niepoprawna liczbe")
         }
         print("Podaj druga liczbe:")
         guard let b = Double(readLine()!) else {
-            fatalError("Podaj poprawna liczbe")
+            fatalError("Podano niepoprawna liczbe")
         }
+
         while true {
             print("""
                   1 - dodawanie
@@ -67,34 +71,47 @@ struct lab_4 {
                   4 - dzielenie
                   """)
 
-            switch Int(readLine()!)! {
+            guard let choice = Double(readLine()!) else {
+                print("Takiej opcji nie ma w menu")
+                continue
+            }
+
+            switch choice {
             case 1: print(a + b)
             case 2: print(a - b)
             case 3: print(a * b)
             case 4 where b != 0: print(a / b)
             case 4: print("Dzielenie przez zero")
-            default: print("Bledny wybor. Sprobuj ponownie.")
+            default: print("Bledny wybor, sprobuj ponownie.")
             }
+            print()
         }
     }
 
     static func task_5() {
         print("Podaj kod pocztowy:")
-        var region: String
+        let postalCode = readLine()!
+        if postalCode.count != 5 && postalCode.count != 6 {
+            fatalError("Podany kod jest zlej dlugosci")
+        }
+        guard let firstChar = postalCode.first else {
+            fatalError("Nie udalo sie uzyskac pierwszego znaku")
+        }
 
-        switch readLine()!.first! {
-        case "0": region = "mazowieckie"
-        case "1": region = "warminsko-mazurskie"
-        case "2": region = "lubelskie"
-        case "3": region = "malopolskie"
-        case "4": region = "slaskie"
-        case "5": region = "dolnoslaskie"
-        case "6": region = "wielkopolskie"
-        case "7": region = "zachodniopomorskie"
-        case "8": region = "pomorskie"
+        let region: String
+        switch firstChar {
+        case "0": region = "warszawskie"
+        case "1": region = "olsztynskie i bialostockie"
+        case "2": region = "lubelskie i kieleckie"
+        case "3": region = "krakowskie i rzeszowskie"
+        case "4": region = "katowickie i opolskie"
+        case "5": region = "wroclawskie"
+        case "6": region = "poznanskie i zielonogorskie"
+        case "7": region = "szczecinskie i koszalinskie"
+        case "8": region = "gdanskie i bydgoskie"
         case "9": region = "lodzkie"
         default:
-            fatalError()
+            fatalError("Wprowadzono bledne dane")
         }
 
         print("Wojewodztwo: \(region)")
@@ -102,9 +119,13 @@ struct lab_4 {
 
     static func task_6() {
         print("Podaj znak z klawiatury")
-        var result: String
+        let x = readLine()!
+        if x.count > 1 {
+            fatalError("Podano wiecej niz jeden znak")
+        }
 
-        switch Character(readLine()!) {
+        var result: String
+        switch x {
         case "a", "e", "i", "o", "u", "y":
             result = "samogloska"
         case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "w", "z":
@@ -120,7 +141,13 @@ struct lab_4 {
     static func task_7() {
         print("Podaj PESEL:")
         let pesel = readLine()!
-        let x = Int(String(pesel[pesel.index(pesel.endIndex, offsetBy: -2)]))
+        if pesel.count != 11 {
+            fatalError("Niepoprawna dlugosc PESEL")
+        }
+
+        guard let x = Int(String(pesel[pesel.index(pesel.endIndex, offsetBy: -2)])) else {
+            fatalError("Niepoprawny format PESEL")
+        }
 
         switch x {
         case 1, 3, 5, 7, 9:
