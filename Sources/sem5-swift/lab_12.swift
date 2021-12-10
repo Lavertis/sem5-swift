de670370bed72464c8c545c80e84a55064df9318
lavertis <29688908+Lavertis@users.noreply.github.com>
2021-12-10 22:14:56 +0100

    feat(lab_12): task_2 finished

--- Sources/sem5-swift/lab_12.swift
+++ Sources/sem5-swift/lab_12.swift
@@ -1,11 +1,15 @@
 import Foundation
 
-protocol LiczbaZespolona {
+protocol ComplexNum {
     var re: Double { get set }
     var im: Double { get set }
 }
 
-class OperacjeNaLiczbachZespolonych: LiczbaZespolona {
+protocol RandomNum {
+    func generateRandomNum() -> Int
+}
+
+class ComplexNumOperations: ComplexNum {
     var re: Double
     var im: Double
 
@@ -14,12 +18,12 @@ class OperacjeNaLiczbachZespolonych: LiczbaZespolona {
         self.im = im
     }
 
-    func dodaj(other: OperacjeNaLiczbachZespolonych) -> OperacjeNaLiczbachZespolonych {
-        OperacjeNaLiczbachZespolonych(re: re + other.re, im: im + other.im)
+    func add(other: ComplexNumOperations) -> ComplexNumOperations {
+        ComplexNumOperations(re: re + other.re, im: im + other.im)
     }
 
-    func odejmij(other: OperacjeNaLiczbachZespolonych) -> OperacjeNaLiczbachZespolonych {
-        OperacjeNaLiczbachZespolonych(re: re - other.re, im: im - other.im)
+    func subtract(other: ComplexNumOperations) -> ComplexNumOperations {
+        ComplexNumOperations(re: re - other.re, im: im - other.im)
     }
 
     func show() {
@@ -31,15 +35,45 @@ class OperacjeNaLiczbachZespolonych: LiczbaZespolona {
     }
 }
 
+class Numbers: RandomNum {
+    var num1: Int = 0
+    var num2: Int = 0
+
+    init() {
+        num1 = generateRandomNum()
+        num2 = generateRandomNum()
+    }
+
+    func generateRandomNum() -> Int {
+        Int.random(in: -20...20)
+    }
+
+    func show() {
+        print(num1, num2)
+    }
+
+    func myPow(power: Int) {
+        num1 = Int(pow(Double(num1), Double(power)))
+        num2 = Int(pow(Double(num2), Double(power)))
+    }
+}
+
 struct lab_12 {
     static func task_1() {
-        let num1 = OperacjeNaLiczbachZespolonych(re: 3.5, im: -4.5)
-        let num2 = OperacjeNaLiczbachZespolonych(re: -7.5, im: 5)
-        let sum = num1.dodaj(other: num2)
-        let difference = num1.odejmij(other: num2)
+        let num1 = ComplexNumOperations(re: 3.5, im: -4.5)
+        let num2 = ComplexNumOperations(re: -7.5, im: 5)
+        let sum = num1.add(other: num2)
+        let difference = num1.subtract(other: num2)
         num1.show()
         num2.show()
         sum.show()
         difference.show()
+    }
+
+    static func task_2() {
+        let numbers = Numbers()
+        numbers.show()
+        numbers.myPow(power: 3)
+        numbers.show()
     }
 }
--- Sources/sem5-swift/main.swift
+++ Sources/sem5-swift/main.swift
@@ -1,3 +1,3 @@
 import Foundation
 
-lab_12.task_1();
+lab_12.task_2();
